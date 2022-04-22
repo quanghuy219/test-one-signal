48c5ed03ec2224edfcacf2ec62082a38d9124494
Huy Pham <p.q.huy97@gmail.com>
2022-04-22 14:17:12 +0700

    Register external user id

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { useEffect } from "react";
 import "./App.css";
 import OneSignal from "react-onesignal";
 import logo from "./logo.png";
+import { useState } from "react";
 
 function App() {
+  const [externalUserId, setExternalUserId] = useState("")
+
   useEffect(() => {
     OneSignal.init({
       appId: "96c1718d-c4a1-4ce7-8583-59d39cabeaee",
     });
   }, []);
+
 
   return (
     <div className="App">
@@ -26,6 +30,28 @@ function App() {
           Vue
         </button>
       </div>
+
+      <form>
+        <label htmlFor="externalUserId">External User ID</label>
+        <input
+          type="text"
+          id="externalUserId"
+          name="externalUserId"
+          onChange={e => setExternalUserId(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Register External User Id"
+          onClick={(e) => {
+            e.preventDefault()
+            console.log(externalUserId)
+            OneSignal.setExternalUserId(externalUserId)
+          }
+          }
+        />
+      </form>
+
+      <button onClick={() => OneSignal.removeExternalUserId()}>Remove Exter User Id</button>
     </div>
   );
 }
